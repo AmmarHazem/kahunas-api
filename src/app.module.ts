e6439eea33f898798190507ed4a071f9f2541b50
Ammar <ammar@ovasave.com>
2025-01-17 23:58:08 +0400

    adding rate limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { SessionsModule } from './sessions/sessions.module';
 import { AnalyticsModule } from './analytics/analytics.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -18,10 +20,22 @@ import { AnalyticsModule } from './analytics/analytics.module';
       synchronize: process.env.NODE_ENV !== 'production',
       logging: ['error', 'warn', 'info'],
     }),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
     AuthModule,
     UsersModule,
     SessionsModule,
     AnalyticsModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
   ],
 })
 export class AppModule {}
